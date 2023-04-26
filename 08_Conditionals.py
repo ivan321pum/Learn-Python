b66d55da85aec59c6414a6fd1356dc67cac35914
ivan321pum <ivansevillagomez@protonmail.com>
2023-04-26 20:05:33 +0200

    Condicionales inicio

---
+++ 08_Conditionals.py
@@ -0,0 +1,17 @@
+# Condicionales #
+my_condition = True
+my_other_condition = "xd"
+
+if my_condition == False:
+    print("Es falso!!")
+
+
+if my_other_condition != True or False:  # también se puede poner type(bool)
+    print("Es true")
+
+mi_number = 5
+
+if mi_number >= 10:
+    print("Es mayor o igual que 10")
+else:
+    print("Es menor que 10")
