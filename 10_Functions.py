03d0ef32ff6d52011c79581a803860d5362eeb58
ivan321pum <ivansevillagomez@protonmail.com>
2023-05-07 12:49:56 +0200

    Creación file classes

--- 10_Functions.py
+++ 10_Functions.py
@@ -1,4 +1,6 @@
 """Functions"""
+
+
 def my_function():
     mis_mierdas = "Caca"
     if mis_mierdas == "Caca":
@@ -9,6 +11,7 @@ my_function()
 my_function()
 my_function()
 
+
 def sum_two_values(value_1, value_2):
     if type(value_1) and type(value_2) == int:
         print(value_1 + value_2)
@@ -18,12 +21,14 @@ def sum_two_values(value_1, value_2):
 
 sum_two_values(6, 4)
 
+
 def sum_two_values_with_return(value_1, value_2):
     return value_1 + value_2
 
 
 my_result = sum_two_values_with_return(3, 6)  # puedo guardarme lo que devuelve con return
 print(my_result)
+
 
 def print_name (name, surname):
     print(f"Mi nombre es {name} y mi apellido {surname}")
@@ -38,8 +43,10 @@ def print_name_with_default(name, surname, alias = "Sin alias"):  # esto hace qu
 
 print_name_with_default("Iván", "Sevilla")
 
+
 def print_texts(*texts):  # poniendo el arterisco se pueeden poner todos los parametros que quieras
     for text in texts:
         print(text)
+
 
 print_texts("Hola", "Como", "Estas", "Estos", "Son", "Varios", "Parametros")
---
+++ 11_Classes.py
@@ -0,0 +1,8 @@
+"""Classes"""
+
+
+class Person:  # nota: escribir las clases con la primera en mayuscula por ejemplo MyPerson
+    pass  # esto lo que hace que no de error cuando no le pones nada a a la clase o función
+
+
+print(Person)
