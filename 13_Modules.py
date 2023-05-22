8adea169f8922268a471304c54c0ba1fab519046
Iván Sevilla Gómez <ivansevillagomez@protonmail.com>
2023-05-22 19:58:17 +0200

    Finalización

---
+++ 13_Modules.py
@@ -0,0 +1,23 @@
+### Modules ###
+
+from math import pi as PI_VALUE
+import math
+from module import sum, printValue
+import module
+
+module.sum(5, 3, 1)
+module.printValue("Hola Python!")
+
+
+sum(5, 3, 1)
+printValue("Hola python")
+
+
+print(math.pi)
+print(math.pow(2, 8))
+pi = math.pi
+
+print(PI_VALUE)
+print(math.isfinite(pi))
+
+print(math.cos(35))
---
+++ module.py
@@ -0,0 +1,8 @@
+
+def sum(numberOne, numberTwo, numberThree):
+    print(numberOne + numberTwo + numberThree)
+
+
+def printValue(value_to_print):
+    print(value_to_print)
+
