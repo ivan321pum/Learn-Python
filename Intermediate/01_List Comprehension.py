d5d8068168a7201c124c2403f0b6bdc98935ca65
Iván Sevilla Gómez <ivansevillagomez@protonmail.com>
2023-06-02 19:35:53 +0200

    Reestructuración para intermedio

--- Intermediate/01_List Comprehension.py
+++ Intermediate/01_List Comprehension.py
@@ -1,2 +1,10 @@
 """List Comprehension"""
 
+my_original_list = [1, 2, 3, 4, 5, 6, 7]
+print(my_original_list)
+
+my_list = [i for i in range(8)]
+print(my_list)
+
+my_range = range(8)
+print(list(my_range))
