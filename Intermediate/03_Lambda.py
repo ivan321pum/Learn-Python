9c3122dc88182d8afe08fa5e49134b1cb5445c64
Iván Sevilla Gómez <ivansevillagomez@protonmail.com>
2023-06-04 16:41:08 +0200

    Lambdas finish

--- Intermediate/03_Lambda.py
+++ Intermediate/03_Lambda.py
@@ -1,2 +1,13 @@
 """Lambdas"""
 
+sum_two_values = lambda num1, num2: print(num1 + num2)
+print(sum_two_values(2, 4))
+
+multiply_values = lambda first_value, second_value: first_value * second_value -3
+print(multiply_values(5, 4))
+
+def sum_three_values(value):
+    return lambda first_value, second_value: first_value + second_value + value
+
+
+print(sum_three_values(5)(3, 5))
