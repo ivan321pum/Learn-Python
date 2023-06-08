20c396ee619c6677feb72486c623248ea692b297
Iván Sevilla Gómez <ivansevillagomez@protonmail.com>
2023-06-08 19:27:28 +0200

    High order functions

--- Intermediate/04_high_order_functions.py
+++ Intermediate/04_high_order_functions.py
@@ -1,4 +1,5 @@
 """High order functions"""
+from functools import reduce
 
 
 def sum_one(value):
@@ -19,12 +20,48 @@ print(sum_two_values_and_add_one(5, 2, sum_five))
 """Closures"""
 
 
-def sum_ten():
+def sum_ten(original_value):
     def add(value):
-        return value + 10
+        return value + 10 + original_value
+
     return add
 
 
-print(sum_ten()(10))
-add_closure = sum_ten()
+print(sum_ten(3)(10))
+add_closure = sum_ten(1)
 print(add_closure(10))
+
+"""Built-In High order functions"""
+
+numbers = [2, 5, 10, 21, 3, 30]
+
+# Map
+
+
+def multiply_by_two(number):
+    return number * 2
+
+
+print(list(map(lambda number: number * 2, numbers)))
+
+# Filter
+
+
+def filter_greater_ten(number):
+    if number > 10:
+        return True
+    else:
+        return False
+
+
+print(list(filter(filter_greater_ten, numbers)))
+print(list(filter(lambda number: number > 10, numbers)))
+
+#Reduce
+
+
+def sum_two_values(value1, value2):
+    return value1 + value2
+
+
+print(reduce(sum_two_values, numbers))
