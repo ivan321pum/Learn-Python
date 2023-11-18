1c6af31ae22405a2003f0e4b91715cf6a95bc510
Iván Sevilla Gómez <ivansevillagomez@protonmail.com>
2023-11-18 13:30:47 +0100

    Cositas

---
+++ Backend/FastApi/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+"""para ejecutar el servidor local en uvicorn hay que poner en terminal: 
+uvicorn main(nombre del fichero):app(nombre de la variable a la que hemos asignado fastapi) --reload(para que se recargue automaticamente
+"""
+
+@app.get("/") #/ es para ponerlo en la raíz, pero podrias ponerlo en cualquier otra página
+async def root():  # Se pone asíncrono para no parar todo el programa en lo que tarda en hacer la petición del backend
+    return ("¡Hola mundo!"
+
+
+@app.get("/"))
+async def home():
+    return "¡Bienvenido a casa!"
---
+++ Backend/type_hints.py
@@ -0,0 +1,13 @@
+"""Type Hints"""
+
+my_string_variable = "My string variable"
+print(my_string_variable)
+print(type(my_string_variable))
+
+my_string_variable = 5
+print(my_string_variable)
+print(type(my_string_variable))
+
+my_typed_variable: str = "my typed string invariable"
+print(my_typed_variable)
+print(type(my_typed_variable))
